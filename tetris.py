e8a607761e7393adc23f4f5373cf65d9063dc3be
aniket209 <patilaniket209@gmail.com>
2023-10-23 21:24:58 +0530

    First update

--- tetris.py
+++ tetris.py
@@ -146,8 +146,15 @@ class Piece(object):
         self.color = shape_colors[shapes.index(shape)]
         self.rotation = 0
  
-def create_grid(locked_positions={}):
-    pass
+def create_grid(locked_pos={}):
+    grid = [[(0,0,0)for x in range(10)] for x in range(20)]
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if (j,i) in locked_pos:
+                c = locked_pos[(j,i)]
+                grid[i][j] = c 
+    return grid
  
 def convert_shape_format(shape):
     pass
