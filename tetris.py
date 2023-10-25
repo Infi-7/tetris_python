4b39ef91534bfa92b856bf8a0f1ca3c902cfb082
aniket209 <patilaniket209@gmail.com>
2023-10-25 20:02:42 +0530

    Third day rev 3

--- tetris.py
+++ tetris.py
@@ -157,8 +157,21 @@ def create_grid(locked_pos={}):
     return grid
  
 def convert_shape_format(shape):
-    pass
- 
+    positions = []
+    format = shape.shape[shape.rotation % len(shape.shape)]
+
+    for i,line in enumerate(format):
+        row = list(line)
+        for j,column in enumerate(row):
+            if column == 0:
+                positions.append((shape.x + j, shape.y + i))
+
+
+    for i,pos in enumerate(positions):
+        positions[i] = (pos[0] - 2, pos[1] - 4)
+
+
+
 def valid_space(shape, grid):
     pass
  
@@ -172,16 +185,18 @@ def get_shape():
 def draw_text_middle(text, size, color, surface):
     pass
    
-def draw_grid(surface, row, col):
+def draw_grid(surface, grid):
+    sx = top_left_x
+    sy = top_left_y
 
     for i in range(len(grid)):
+        pygame.draw.line(surface, (128,128,128), (sx, sy+i*block_size),(sx+play_width, sy+i*block_size))
         for j in range(len(grid[i])):
-            pygame.draw.rect(surface, grid[i][j], (top_left_x + j*block_size ,top_left_y + i*block_size, block_size, block_size), 0)
-
-    pygame.draw.rect(surface, (255,0,0),(top_left_x, top_left_y, play_width, play_height))
+            pygame.draw.line(surface, (128,128,128), (sx + j*block_size, sy),(sx + j*block_size, sy + play_height))
+    
 
 def clear_rows(grid, locked):
- 
+
  
 def draw_next_shape(shape, surface):
  
@@ -194,6 +209,12 @@ def draw_window(surface,grid):
     label = font.render('Tetris',1,(255,255,255))
 
     surface.blit(label, (top_left_x + play_width/2 - (label.getwidth())/2 , 30))
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            pygame.draw.rect(surface, grid[i][j], (top_left_x + j*block_size ,top_left_y + i*block_size, block_size, block_size), 0)
+
+    pygame.draw.rect(surface, (255,0,0),(top_left_x, top_left_y, play_width, play_height))
+
     draw_grid(surface, grid)
     pygame.display.update()
  
